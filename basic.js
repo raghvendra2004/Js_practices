f2e6685f747e65b967e4b7d5ce48d4d5a0c7f25c
Raghvendra <119332374+raghvendra2004@users.noreply.github.com>
2024-02-10 18:45:59 +0000

    success

--- basic.js
+++ basic.js
@@ -1,1 +1,3 @@
-
+let name=1
+let name1=2
+console.log(`this is name of one person is ${name} and second is ${name1}`)
---
+++ basic1.js
@@ -0,0 +1,6 @@
+let mydate= new Date()
+// console.log(mydate)
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString);
+let TimeStamp=Date.now()
+console.log(TimeStamp);
