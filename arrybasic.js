3b01202b4543b2e381cc37f1f136939f48a8757b
Raghvendra <119332374+raghvendra2004@users.noreply.github.com>
2024-02-11 17:33:19 +0000

    suceess

--- arrybasic.js
+++ arrybasic.js
@@ -3,3 +3,8 @@ const myarry1=[0,1,4,7]
 //console.log(myarry)
 // myarry.push(myarry1)
 // console.log(myarry)
+let score1=100
+let score2=2
+console.log(Array.from("raghvendra"))
+console.log(Array.of(score1,score2));
+
